a47279fea6b66d2f9669da554013e092b518b146
JaxonWright <jaxonhwright@gmail.com>
2017-04-05 16:13:06 -0400

    Add Theme Colors, Branding logos, Initial Firebase Setup.

--- app/src/main/java/com/dunrite/tallyup/MainActivity.java
+++ app/src/main/java/com/dunrite/tallyup/MainActivity.java
@@ -1,7 +1,13 @@
 package com.dunrite.tallyup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRecentsStyle();
+    }
+
+    /**
+     * Sets custom white logo on recent app menu card
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setRecentsStyle() {
+        Bitmap recentsIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_white);
+        String title = "TallyUp";
+        int color = ContextCompat.getColor(this, R.color.colorPrimary);
+
+        ActivityManager.TaskDescription description =
+                new ActivityManager.TaskDescription(title, recentsIcon, color);
+        this.setTaskDescription(description);
     }
 }
