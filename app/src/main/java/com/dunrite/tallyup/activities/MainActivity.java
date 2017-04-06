903ff268c32c0070ecf168acfc02354e87c43317
JaxonWright <jaxonhwright@gmail.com>
2017-04-05 22:30:37 -0400

    Add butterknife. Add FAB to MainActivity that launches CreateActivity

--- app/src/main/java/com/dunrite/tallyup/activities/MainActivity.java
+++ app/src/main/java/com/dunrite/tallyup/activities/MainActivity.java
@@ -7,27 +7,40 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.dunrite.tallyup.R;
 import com.dunrite.tallyup.utility.Utils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Main activity that shows active and closed polls for the user
  */
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.fab_create) FloatingActionButton fabCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         setRecentsStyle();
 
         if(Utils.isFirstLaunch(this)) {
             Intent intent = new Intent(this, IntroActivity.class); //call Intro class
             startActivity(intent);
         }
+    }
+
+    @OnClick(R.id.fab_create)
+    public void onClickFab() {
+        Intent intent = new Intent(this, CreateActivity.class); //call Intro class
+        startActivity(intent);
     }
 
     /**
