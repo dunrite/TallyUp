9c3766fd438c708a0437fe21fb7d429b5899590c
JaxonWright <jaxonhwright@gmail.com>
2017-04-17 21:52:05 -0400

    Allow user to select choice type and expire time when creating a poll

--- app/src/main/java/com/dunrite/tallyup/activities/CreateActivity.java
+++ app/src/main/java/com/dunrite/tallyup/activities/CreateActivity.java
@@ -6,8 +6,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.CheckBox;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.dunrite.tallyup.R;
@@ -17,12 +19,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import mobi.upod.timedurationpicker.TimeDurationPicker;
+import mobi.upod.timedurationpicker.TimeDurationPickerDialog;
 import shortbread.Shortcut;
 
 /**
@@ -32,7 +38,9 @@ import shortbread.Shortcut;
 public class CreateActivity extends FirebaseActivity {
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.questionText) EditText question;
-    @BindView(R.id.multiSelect) CheckBox multiSelect;
+    //@BindView(R.id.multiSelect) CheckBox multiSelect;
+    @BindView(R.id.type_spinner) Spinner typeSpinner;
+    @BindView(R.id.timeDurationButton) Button timeButton;
     @BindView(R.id.item0) EditText item0;
     @BindView(R.id.item1) EditText item1;
     @BindView(R.id.item2) EditText item2;
@@ -40,6 +48,7 @@ public class CreateActivity extends FirebaseActivity {
     @BindView(R.id.confirm_create_fab) FloatingActionButton fab;
     private String formattedQuestion;
     private String pollID;
+    private long durationInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +57,25 @@ public class CreateActivity extends FirebaseActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        durationInMillis = 600000;
+        formatLengthButton(durationInMillis);
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.choice_types, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(spinnerAdapter);
+    }
+
+    @OnClick(R.id.timeDurationButton)
+    public void onClickTimeButton() {
+        TimeDurationPickerDialog dialog = new TimeDurationPickerDialog(this, new TimeDurationPickerDialog.OnDurationSetListener() {
+            @Override
+            public void onDurationSet(TimeDurationPicker view, long duration) {
+                durationInMillis = duration;
+                formatLengthButton(durationInMillis);
+            }
+        }, durationInMillis);
+        dialog.show();
     }
 
     @OnClick(R.id.confirm_create_fab)
@@ -63,6 +91,20 @@ public class CreateActivity extends FirebaseActivity {
             s = s + "?";
         }
         return s;
+    }
+
+    private void formatLengthButton(long millis) {
+        long second = (millis / 1000) % 60;
+        long minute = (millis / (1000 * 60)) % 60;
+        long hour = (millis / (1000 * 60 * 60)) % 24;
+        String time = String.format("Length: %02dh %02dm %02ds", hour, minute, second);
+        timeButton.setText(time);
+    }
+
+    private String millisFromNow(long millis) {
+        Date now = Calendar.getInstance().getTime();
+        now.setTime(now.getTime() + millis);
+        return java.text.DateFormat.getDateTimeInstance().format(now);
     }
 
     private boolean validQuestion() {
@@ -93,8 +135,9 @@ public class CreateActivity extends FirebaseActivity {
         newQuestion.put("Question", formattedQuestion);
         newQuestion.put("Item0", i0);
         newQuestion.put("Item1", i1);
-
         newQuestion.put("OwnerID", mAuth.getCurrentUser().getUid());
+        newQuestion.put("Type", typeSpinner.getSelectedItem().toString());
+        newQuestion.put("ExpireTime", millisFromNow(durationInMillis));
         mDatabase = FirebaseDatabase.getInstance().getReference("Polls");
         pollID = Utils.generateSaltString();
         mDatabase.child(pollID).updateChildren(newQuestion).addOnCompleteListener(new OnCompleteListener<Void>() {
